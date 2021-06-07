8b3315e16986d84f3c1b8af85c3e5813919a72dd
YukaO614 <okabe.yuka0614@gmail.com>
2021-06-07 02:38:53 +0000

    Railsコース - Herokuへのデプロイ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {maximum:15}
-    validates :email, presence: true, format: {with: /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/}
-    validates :password, presence: true, length: {minimum: 8, maximum: 32}, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+    validates :email, presence: true, format: {with: /([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+\.([a-zA-Z0-9\._-]+)/}
+    validates :password, presence: true, format: {with: /(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}/}
     
     has_secure_password
     
